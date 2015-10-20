297556e4b329de013ec34d37fe1eef55754884a6
DongLi <jellylidong2nd@gmail.com>
2015-10-20 02:42:46 -0400

    shortestDistance two pointer method

--- Array/Problem243_Shortest_Word_Distance.java
+++ Array/Problem243_Shortest_Word_Distance.java
@@ -32,4 +32,19 @@ public class Problem243_Shortest_Word_Distance {
         }
         return res;
     }
+	
+	public int shortestDistance_method2(String[] words, String word1, String word2){
+		int p1 = -1; 
+		int p2 = -1;
+		int res = Integer.MAX_VALUE;
+		for(int i = 0; i < words.length; i++){
+			if(words[i].equals(word1))
+				p1 = i;
+			if(words[i].equals(word2))
+				p2 = i;
+			if(p1 != -1 && p2 != -1)
+				res = Math.min(res, Math.abs(p1 - p2));
+		}
+		return res;
+	}
 }
